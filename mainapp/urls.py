3dccb0861382dea40985d86c824bcb87bbcb3fc3
chamorcev <chamorcev@gmail.com>
2021-11-01 11:45:52 +0300

    HW8 Перевел категории на классы, продукты оставил как есть.

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,7 +4,9 @@ from .views import products, product
 app_name = 'mainapp'
 
 urlpatterns = [
-    path('', products, name='products'),
+    path('', products, name='main'),
     path('category/<int:pk>', products, name='category'),
     path('product/<int:pk>', product, name='detail'),
+    path('category/<int:pk>/page/<int:page>/', products, name='page'),
+
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,6 @@
 import random
 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 
 from basketapp.models import Basket
@@ -11,38 +12,54 @@ def get_basket(user):
     else:
         return []
 
+
 def get_hot_product():
     products = Product.objects.all()
     return random.sample(list(products),1)[0]
+
 
 def get_same_products(hot_product):
     same_products = Product.objects.filter(category=hot_product.category).exclude(pk=hot_product.pk)[:3]
     return same_products
 
 
-def products(request, pk=None):
+def products(request, pk=None, page=1):
     title = 'Каталог'
     lname = 'products'
     links_menu = ProductCategory.objects.all()
-    products = Product.objects.all().order_by('price')
+    # products = Product.objects.all().order_by('price')
     basket = get_basket(request.user)
-
 
     if pk is not None:
         if pk == 0:
-            products = Product.objects.all().order_by('price')
-            category = {'name': 'все'}
-            basket = Basket.objects.filter(user=request.user)
+            # products = Product.objects.all().order_by('price')
+            # category = {'name': 'все'}
+            # basket = Basket.objects.filter(user=request.user)
+            category = {'pk': 0, 'name': 'все'}
+            products = Product.objects.filter(is_active=True, category__is_active=True).order_by('price')
+
         else:
             category = get_object_or_404(ProductCategory, pk=pk)
-            products = Product.objects.filter(category__pk=pk).order_by('price')
-            basket = Basket.objects.filter(user=request.user)
+            products = Product.objects.filter(
+                category__pk=pk,
+                is_active=True,
+                category__is_active=True
+            ).order_by('price')
+            # basket = Basket.objects.filter(user=request.user)
+
+        paginator = Paginator(products, 2)
+        try:
+            products_paginator = paginator.page(page)
+        except PageNotAnInteger:
+            products_paginator = paginator.page(1)
+        except EmptyPage:
+            products_paginator = paginator.page(paginator.num_pages)
 
         context = {
             'title': title,
             'links_menu': links_menu,
             'lname': lname,
-            'products': products,
+            'products': products_paginator,
             'category': category,
             'basket': basket,
             'basket_count': basket.count(),
@@ -53,6 +70,7 @@ def products(request, pk=None):
 
     hot_product = get_hot_product()
     same_products = get_same_products(hot_product)
+    products = Product.objects.filter(is_active=True, category__is_active=True).order_by('price')
     context = {
         'title': title,
         'links_menu': links_menu,
